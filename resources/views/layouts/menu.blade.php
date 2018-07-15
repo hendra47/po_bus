aea30d894ddd27427c6f0348e1cc3ccbae84bae4
hendraw <mailhendra2@gmail.com>
2018-07-15 14:44:27 +0700

    update

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,49 @@
+
+<li>
+    <a href="{!! url('/home') !!}"><i class="fa fa-home"></i><span>Menu Utama</span></a>
+</li>
+
+@if(App\Helpers\spkHelper::akses('user')==='true')
+<li class="{{ Request::is('users*') ? 'active' : '' }}">
+    <a href="{!! route('users.index') !!}"><i class="fa fa-user"></i><span>Data Users</span></a>
+</li>
+@endif
+
+@if(App\Helpers\spkHelper::akses('akses')==='true')
+<!-- <li class="{{ Request::is('hakAkses*') ? 'active' : '' }}">
+    <a href="{!! route('hakAkses.index') !!}"><i class="fa fa-wrench"></i><span>Hak Akses</span></a>
+</li>  -->
+@endif
+
+<!--
+@if(App\Helpers\spkHelper::akses('laporan')==='true')
+<li class="{{ Request::is('laporan*') ? 'active treeview' : 'treeview' }}">
+    <a href="#">
+    <i class="fa fa-bar-chart"></i>
+    <span>Laporan Penilaian </span>
+    <span class="pull-right-container">
+        <i class="fa fa-angle-left pull-right"></i>
+    </span>
+    </a>
+    <ul class="treeview-menu">
+    <li class="{{ Request::segment(2)==1 ? 'active' : '' }}"><a href="{!! URL::route('lap',1) !!}"><i class="fa fa-circle-o"></i>Data Nilai siswa</a></li>
+    <li class="{{ Request::segment(2)==2 ? 'active' : '' }}"><a href="{!! URL::route('lap',2) !!}"><i class="fa fa-circle-o"></i>Hasil Analisa</a></li>
+    <li class="{{ Request::segment(2)==3 ? 'active' : '' }}"><a href="{!! URL::route('lap',3) !!}"><i class="fa fa-circle-o"></i>Hasil Normalisasi</a></li>
+    <li class="{{ Request::segment(2)==4 ? 'active' : '' }}"><a href="{!! URL::route('lap',4) !!}"><i class="fa fa-circle-o"></i>Hasil Perangkingan</a></li>
+    <li class="{{ Request::segment(2)==5 ? 'active' : '' }}"><a href="{!! URL::route('lap',5) !!}"><i class="fa fa-circle-o"></i>Hasil Keputusan</a></li>    
+    </ul>
+</li>
+@endif-->
+
+
+<li>
+    <a href="{!! url('/logout') !!}"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-sign-out"></i><span>Sign Out</span></a>
+    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
+                                          style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+</li>
+
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+  return redirect('home');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::resource('users', 'userController');
+
+Route::resource('hakAkses', 'hakAksesController');
+
+Route::get('crips-new/{id}', ['as' => 'cripsNew', 'uses' => 'cripsController@CreateNew']);
+Route::get('input-nilai/{id}/{kode}/{kriteria}/{kode_kriteria}', ['as' => 'inputNilai', 'uses' => 'nilai_alternatifController@inputNilai']);
+
+Route::get('ajax/{nilai}/{kode}/{id}', ['as' => 'ajax', 'uses' => 'nilai_alternatifController@ajax']);
+
+Route::get('laporan/{id}', ['as' => 'lap', 'uses' => 'lapController@index']);
+
