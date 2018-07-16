66a380aa36ee5d6aeef6ac2f714322a250efcac8
unknown <arisyunanto.bp20@gmail.com>
2018-07-16 19:35:07 +0700

    upd

--- po_dewisri.sql
+++ po_dewisri.sql
@@ -1,139 +1,126 @@
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: po_dewisri
--- ------------------------------------------------------
--- Server version	5.6.40
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2018 at 02:33 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `po_dewisri`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `bus`
 --
 
-DROP TABLE IF EXISTS `bus`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bus` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `no_bus` char(30) DEFAULT NULL,
   `kapasitas` int(11) DEFAULT NULL,
   `fasilitas` varchar(255) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `bus`
 --
 
-LOCK TABLES `bus` WRITE;
-/*!40000 ALTER TABLE `bus` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bus` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `bus` (`id`, `no_bus`, `kapasitas`, `fasilitas`, `keterangan`, `created_at`, `updated_at`) VALUES
+(1, '3243', 124324, 'sad', 'dsvd', '2018-07-15 09:45:43', '2018-07-15 09:45:43');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `hak_akses`
 --
 
-DROP TABLE IF EXISTS `hak_akses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `hak_akses` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL,
   `group` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `menu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `keterangan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `hak_akses`
 --
 
-LOCK TABLES `hak_akses` WRITE;
-/*!40000 ALTER TABLE `hak_akses` DISABLE KEYS */;
-INSERT INTO `hak_akses` VALUES (1,'admin','user,akses,siswa,alternatif,kriteria,crips,penilaian,laporan','admin full akses','2017-11-18 03:20:03','2017-12-03 05:56:12'),(2,'kesiswaan','penilaian','-','2017-12-03 04:45:45','2017-12-09 07:28:59'),(3,'walikelas','penilaian','-','2017-12-03 04:45:57','2017-12-03 05:14:28'),(11,'kepala_sekolah','laporan','-','2017-12-03 05:13:40','2017-12-03 05:13:40');
-/*!40000 ALTER TABLE `hak_akses` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `hak_akses` (`id`, `group`, `menu`, `keterangan`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'user,akses,siswa,alternatif,kriteria,crips,penilaian,laporan', 'admin full akses', '2017-11-18 03:20:03', '2017-12-03 05:56:12'),
+(2, 'kesiswaan', 'penilaian', '-', '2017-12-03 04:45:45', '2017-12-09 07:28:59'),
+(3, 'walikelas', 'penilaian', '-', '2017-12-03 04:45:57', '2017-12-03 05:14:28'),
+(11, 'kepala_sekolah', 'laporan', '-', '2017-12-03 05:13:40', '2017-12-03 05:13:40');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `harga`
 --
 
-DROP TABLE IF EXISTS `harga`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `harga` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `nominal` float DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `harga_lama` float DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `harga_lama` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `harga`
 --
 
-LOCK TABLES `harga` WRITE;
-/*!40000 ALTER TABLE `harga` DISABLE KEYS */;
-INSERT INTO `harga` VALUES (1,9000,'abc','2018-07-15 08:52:05','2018-07-15 08:52:05',8000);
-/*!40000 ALTER TABLE `harga` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `harga` (`id`, `nominal`, `keterangan`, `created_at`, `updated_at`, `harga_lama`) VALUES
+(1, 9000, 'abc', '2018-07-15 08:52:05', '2018-07-15 08:52:05', 8000);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `jam`
 --
 
-DROP TABLE IF EXISTS `jam`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `jam` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `jam` varchar(30) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `jam`
 --
 
-LOCK TABLES `jam` WRITE;
-/*!40000 ALTER TABLE `jam` DISABLE KEYS */;
-/*!40000 ALTER TABLE `jam` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `jam` (`id`, `jam`, `keterangan`, `created_at`, `updated_at`) VALUES
+(2, '00:00', 'dwd', '2018-07-16 08:27:07', '2018-07-16 08:27:56');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `kategori`
 --
 
-DROP TABLE IF EXISTS `kategori`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `kategori` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `nama` char(30) DEFAULT NULL,
   `id_kota` int(11) DEFAULT NULL,
   `id_jam` int(11) DEFAULT NULL,
@@ -141,135 +128,111 @@ CREATE TABLE `kategori` (
   `id_harga` int(11) DEFAULT NULL,
   `id_bus` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `kategori`
 --
 
-LOCK TABLES `kategori` WRITE;
-/*!40000 ALTER TABLE `kategori` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kategori` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `kategori` (`id`, `nama`, `id_kota`, `id_jam`, `id_rute`, `id_harga`, `id_bus`, `created_at`, `updated_at`) VALUES
+(2, 'aku', 4, 3, 6, 4, 7, '2018-07-15 14:27:06', '2018-07-15 14:27:06');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `kota`
 --
 
-DROP TABLE IF EXISTS `kota`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `kota` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `nama` char(30) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  `update_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `kota`
---
-
-LOCK TABLES `kota` WRITE;
-/*!40000 ALTER TABLE `kota` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kota` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `member`
 --
 
-DROP TABLE IF EXISTS `member`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `member` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `nama` char(30) DEFAULT NULL,
   `no_ktp` varchar(16) DEFAULT NULL,
   `tgl_lahir` date DEFAULT NULL,
   `jk` char(1) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
-  `created_at` int(11) DEFAULT NULL,
-  `update_at` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `member`
 --
 
-LOCK TABLES `member` WRITE;
-/*!40000 ALTER TABLE `member` DISABLE KEYS */;
-/*!40000 ALTER TABLE `member` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `member` (`id`, `nama`, `no_ktp`, `tgl_lahir`, `jk`, `alamat`, `created_at`, `updated_at`) VALUES
+(1, 'ergt', '54354', '1970-01-01', 'L', 'grth', '2018-07-15 09:14:55', '2018-07-15 09:14:55');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `migrations`
 --
 
-DROP TABLE IF EXISTS `migrations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `migrations` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL,
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
 --
 
-LOCK TABLES `migrations` WRITE;
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2017_08_19_102739_create_users_table',1),(9,'2017_11_18_091937_create_siswas_table',2),(11,'2017_11_18_100432_create_hak_akses_table',4),(12,'2017_12_01_132638_create_kriterias_table',5),(13,'2017_12_02_034250_create_kriterias_table',6),(14,'2017_12_02_034551_create_kriterias_table',7),(15,'2017_12_02_042254_create_crips_table',8),(16,'2017_12_02_070929_create_alternatifs_table',9),(17,'2017_12_02_075551_create_nilai_alternatifs_table',10);
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2017_08_19_102739_create_users_table', 1),
+(9, '2017_11_18_091937_create_siswas_table', 2),
+(11, '2017_11_18_100432_create_hak_akses_table', 4),
+(12, '2017_12_01_132638_create_kriterias_table', 5),
+(13, '2017_12_02_034250_create_kriterias_table', 6),
+(14, '2017_12_02_034551_create_kriterias_table', 7),
+(15, '2017_12_02_042254_create_crips_table', 8),
+(16, '2017_12_02_070929_create_alternatifs_table', 9),
+(17, '2017_12_02_075551_create_nilai_alternatifs_table', 10);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `rute`
 --
 
-DROP TABLE IF EXISTS `rute`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rute` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `rute` varchar(100) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `rute`
 --
 
-LOCK TABLES `rute` WRITE;
-/*!40000 ALTER TABLE `rute` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rute` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `rute` (`id`, `rute`, `keterangan`, `created_at`, `updated_at`) VALUES
+(2, 'df', 'fdfd', '2018-07-16 08:24:58', '2018-07-16 08:28:56'),
+(3, 'fbddf', 'fd', '2018-07-16 08:26:42', '2018-07-16 08:26:42');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `transaksi`
 --
 
-DROP TABLE IF EXISTS `transaksi`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transaksi` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
   `id_kategori` int(11) DEFAULT NULL,
   `tgl_berangkat` datetime DEFAULT NULL,
   `id_member` int(11) DEFAULT NULL,
@@ -277,60 +240,185 @@ CREATE TABLE `transaksi` (
   `total_harga` float DEFAULT NULL,
   `bukti_transfer` varchar(255) DEFAULT NULL,
   `status` char(20) DEFAULT NULL,
-  `created_at` int(11) DEFAULT NULL,
-  `update_at` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `transaksi`
 --
 
-LOCK TABLES `transaksi` WRITE;
-/*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `transaksi` (`id`, `id_kategori`, `tgl_berangkat`, `id_member`, `jml_penumpang`, `total_harga`, `bukti_transfer`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, '2018-09-09 00:00:00', 2, 2, 1000, 'OK', 'y', '2018-07-15 15:10:20', '2018-07-15 15:10:20');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL,
   `nik` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
   `username` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `group` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_nik_unique` (`nik`),
-  UNIQUE KEY `users_username_unique` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'10102','Administrator','admin','$2y$10$Gmkat619O8B3XqmkYUZ8he9N9fBCLq6jUczCHK/Pbppf04O2Vucbi','admin','2017-08-19 09:32:29','2017-08-19 09:32:29'),(5,'10103','Manager','manager','$2y$10$LOBnk4Ntt/aVZ6mTqWgQEOE3pSgKHqp9cEyI3i2W5zmg5vaGSY.Cy','manager','2018-07-15 00:15:57','2018-07-15 00:15:57');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`id`, `nik`, `name`, `username`, `password`, `group`, `created_at`, `updated_at`) VALUES
+(1, '10102', 'Administrator', 'admin', '$2y$10$Gmkat619O8B3XqmkYUZ8he9N9fBCLq6jUczCHK/Pbppf04O2Vucbi', 'admin', '2017-08-19 09:32:29', '2017-08-19 09:32:29'),
+(5, '10103', 'Manager', 'manager', '$2y$10$LOBnk4Ntt/aVZ6mTqWgQEOE3pSgKHqp9cEyI3i2W5zmg5vaGSY.Cy', 'manager', '2018-07-15 00:15:57', '2018-07-15 00:15:57');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bus`
+--
+ALTER TABLE `bus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `harga`
+--
+ALTER TABLE `harga`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jam`
+--
+ALTER TABLE `jam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kota`
+--
+ALTER TABLE `kota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rute`
+--
+ALTER TABLE `rute`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_nik_unique` (`nik`),
+  ADD UNIQUE KEY `users_username_unique` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bus`
+--
+ALTER TABLE `bus`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `harga`
+--
+ALTER TABLE `harga`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `jam`
+--
+ALTER TABLE `jam`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kota`
+--
+ALTER TABLE `kota`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `rute`
+--
+ALTER TABLE `rute`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-07-15 15:53:49
